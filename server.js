22f93d8ae615780164afa3a599835412c21a3aa8
wuxuanbo <43457834@qq.com>
2018-08-31 17:31:40 +0800

    更改默认监听端口

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 9000
+const port = 8001
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(9000);
+app.listen(port);
 console.log('启动服务器成功~listen:' + port)
