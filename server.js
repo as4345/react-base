b52b85c70834f9ffdc56e54b5404c9040916f9e1
wuxuanbo <43457834@qq.com>
2018-09-03 14:37:42 +0800

    增加服务器对gzip支持，去掉生产环境sourcemap配置

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // 用于服务器部署
 const express = require('express');
 const path = require('path');
+const compression = require('compression')
 const app = express();
+// 开启gzip
+app.use(compression());
 const port = 8001
 
 app.use(express.static(path.join(__dirname, 'build')));
